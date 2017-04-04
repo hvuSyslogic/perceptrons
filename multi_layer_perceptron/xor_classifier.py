e42b6bd0a0ea23240fd8e46839f2cab825f992d1
jpazarzis <jpazarzis@gmail.com>
2017-04-04 09:30:35 -0400

    Adding back-propagation.

---
+++ multi_layer_perceptron/xor_classifier.py
@@ -0,0 +1,19 @@
+"""implements a XOR classifier
+"""
+import network
+
+xor_data = [
+      [[1, 1], [0]],
+      [[0, 0], [0]],
+      [[1, 0], [1]],
+      [[0, 1], [1]],
+    ]
+
+
+if __name__ == '__main__':
+  n = network.GetTrainedNetwork(xor_data, 0.001, 8000, True, [2, 4, 1])
+
+  print n.Predict([1, 1])
+  print n.Predict([0, 0])
+  print n.Predict([1, 0])
+  print n.Predict([0, 1])
